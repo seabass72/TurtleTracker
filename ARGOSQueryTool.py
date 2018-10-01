55d8a80472ff0d477333ffd2a0ed03d4c82a5c1a
seabass72 <42747203+seabass72@users.noreply.github.com>
2018-10-01 12:07:02 -0400

    Revert "loops through all lines of the turtle code"
    This reverts commit b7e8fd3d9336cbeac3f2bec0eecaadf9882d3655.

--- ARGOSQueryTool.py
+++ ARGOSQueryTool.py
@@ -1,4 +1,3 @@
-
 # ARGOSQueryTool.py
 #
 # Description: Parses a line of ARGOS tracking data 
@@ -19,20 +18,19 @@ lineList = fileObj.readlines()
 fileObj.close()
 
 # Extract the first line from the lineList
-for lineString in lineList:
-    
+lineString = lineList[0]
 
-    # Use the split command to parse the items in lineString into a list object
-    lineData = lineString.split("\t")
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split("\t")
 
-    # Assign variables to specfic items in the list
-    recordID = lineData[0]              # ARGOS tracking record ID
-    obsDateTime = lineData[2]           # Observation date and time (combined)
-    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-    obsLC = lineData[3]                 # Observation Location Class
-    obsLat = lineData[5]                # Observation Latitude
-    obsLon = lineData[6]                # Observation Longitude
+# Assign variables to specfic items in the list
+recordID = lineData[0]              # ARGOS tracking record ID
+obsDateTime = lineData[2]           # Observation date and time (combined)
+obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+obsLC = lineData[3]                 # Observation Location Class
+obsLat = lineData[5]                # Observation Latitude
+obsLon = lineData[6]                # Observation Longitude
 
-    # Print information to the user
-    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLon,obsDate))
+# Print information to the user
+print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
